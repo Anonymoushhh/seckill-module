e74c75ecd571223523e0c544ccff49c43c48d37f
Anonymoushhh <1765638560@qq.com>
2019-02-21 10:05:15 +0800

    seckill-module

---
+++ src/main/java/com/seckill/service/SeckillService.java
@@ -0,0 +1,54 @@
+package com.seckill.service;
+
+import com.seckill.dto.Exposer;
+import com.seckill.dto.SeckillExecution;
+import com.seckill.entity.Seckill;
+import com.seckill.exception.RepeatKillException;
+import com.seckill.exception.SeckillCloseException;
+import com.seckill.exception.SeckillException;
+
+import java.util.List;
+
+
+public interface SeckillService {
+
+    /**
+     * 查询所有秒杀记录
+     * @return
+     */
+    List<Seckill> getSeckillList();
+
+    /**
+     * 查询单个秒杀记录
+     * @param seckillId
+     * @return
+     */
+    Seckill getById(long seckillId);
+
+    /**
+     * 秒杀开启输出秒杀接口地址,
+     * 否则输出系统时间和秒杀时间
+     *
+     * @param seckillId
+     */
+    Exposer exportSeckillUrl(long seckillId);
+
+    /**
+     * 执行秒杀操作
+     * @param seckillId
+     * @param userPhone
+     * @param md5
+     */
+    SeckillExecution executeSeckill(long seckillId, long userPhone, String md5)
+        throws SeckillException,RepeatKillException,SeckillCloseException;
+
+
+    /**
+     * 执行秒杀操作by 存储过程
+     * @param seckillId
+     * @param userPhone
+     * @param md5
+     */
+    SeckillExecution executeSeckillProcedure(long seckillId, long userPhone, String md5);
+
+}
---
+++ src/test/java/com/seckill/dao/cache/RedisDaoTest.java
@@ -0,0 +1,36 @@
+package com.seckill.dao.cache;
+
+import com.seckill.dao.SeckillDao;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import com.seckill.entity.Seckill;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class RedisDaoTest {
+    private long id = 1001;
+    @Autowired
+    private RedisDao redisDao;
+
+    @Autowired
+    private SeckillDao seckillDao;
+
+    @Test
+    public void testSeckill() throws Exception {
+        //get and put
+        Seckill seckill = redisDao.getSeckill(id);
+        if (seckill == null) {
+            seckill = seckillDao.queryById(id);
+            if(seckill != null){
+                String result = redisDao.putSeckill(seckill);
+                System.out.println(result);
+                seckill = redisDao.getSeckill(id);
+                System.out.println(seckill);
+            }
+
+        }
+    }
+}
